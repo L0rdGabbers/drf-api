a05023e6f3f4aef1c2bc5846023a4d889a5509fe
L0rdGabbers <gabrielmarriage@me.com>
2023-11-15 17:02:03 +0000

    Updates allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['drf-api-gabriel.herokuapp.com',
+ALLOWED_HOSTS = ['drf-api-gabriel-04b9fb83ef08.herokuapp.com',
     '8000-l0rdgabbers-drfapi-vmo3gzcoxzu.ws-eu106.gitpod.io']
 
 
