754027c9699f668a5ed836014565679a0e64cb8d
L0rdGabbers <gabrielmarriage@me.com>
2023-11-15 15:36:53 +0000

    Links up to elephant database and heroku

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -5,16 +6,26 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'post', 'created_at', 'updated_at',
-            'content', 'is_owner',
+            'content', 'is_owner', 'profile_id', 'profile_image'
         ]
 
 class CommentDetailSerializer(CommentSerializer):
